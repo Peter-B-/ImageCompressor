25b4d4372536cdf039d0c5dbd48be96cd555968b
Peter Butzhammer <Peter.Butzhammer@evopro-ag.de>
2021-10-18 17:50:22 +0200

    Add project files.

---
+++ ImageCompressor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageCompressor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
